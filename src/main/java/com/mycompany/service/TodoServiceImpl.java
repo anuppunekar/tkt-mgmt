c02c35dfc8897162ef6c066a2cce00c15dec6e1c
anuppunekar <anup.punekar@gmail.com>
2024-08-17 23:53:16 +0530

    Spring 5.0 User Controller

--- src/main/java/com/mycompany/service/TodoServiceImpl.java
+++ src/main/java/com/mycompany/service/TodoServiceImpl.java
@@ -10,19 +10,23 @@ import java.util.List;
 import java.util.ArrayList;
 import java.util.Date;
 
+import org.springframework.stereotype.Service;
 /**
  *
  * @author 
  * 17-Aug-2024  Added ToDoService 
  */
+@Service
 public class TodoServiceImpl implements TodoService {
     private static List<Todo> todos = new ArrayList<Todo>();
-    private static int iCounttodo = 3;
+    private static int iCounttodo = 5;
     
     static {
         todos.add(new Todo(1, "D", "Learn Spring MVC", new Date(), false));
-        todos.add(new Todo(2, "P", "Learn Struts", new Date(), false));
-        todos.add(new Todo(3, "J", "Learn Hibernate", new Date(), false));
+        todos.add(new Todo(2, "D", "Learn Struts", new Date(), false));
+        todos.add(new Todo(3, "P", "Learn Struts", new Date(), false));
+        todos.add(new Todo(4, "P", "Learn Hibernate", new Date(), false));
+        todos.add(new Todo(5, "J", "Learn Hibernate", new Date(), false));
     }
 
     public List<Todo> retrieveTodos(String user) {
